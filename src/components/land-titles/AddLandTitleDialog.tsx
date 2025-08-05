d4693e541418f9cfb75ceb15c2eb3e183eebdbc6
Timothy Ofie <Boxerbuzz559@gmail.com>
2025-08-05 03:42:06 +0100

    Update AddLandTitleDialog.tsx

--- src/components/land-titles/AddLandTitleDialog.tsx
+++ src/components/land-titles/AddLandTitleDialog.tsx
@@ -16,7 +16,6 @@ import {
 } from "@/components/ui/responsive-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import {
   Select,
