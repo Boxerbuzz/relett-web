c4a153bb9bf9f7454fce5eb806aa3886dc3e922e
Timothy Ofie <Boxerbuzz559@gmail.com>
2025-08-09 13:12:50 +0100

    Delete supabase/migrations directory

--- supabase/migrations/20250808173142_874d509e-dff0-4132-af19-433524306a4f.sql
+++
@@ -1,4 +0,0 @@
--- Add new status values to the tokenization_status enum
-ALTER TYPE tokenization_status ADD VALUE IF NOT EXISTS 'token_created';
-ALTER TYPE tokenization_status ADD VALUE IF NOT EXISTS 'sale_active';
-ALTER TYPE tokenization_status ADD VALUE IF NOT EXISTS 'sale_ended';
--- supabase/migrations/20250808184749_c0c4881a-0f2c-4d8d-9c98-5957487a4fb4.sql
+++
@@ -1,4 +0,0 @@
--- Add sale date columns to tokenized_properties table
-ALTER TABLE public.tokenized_properties 
-ADD COLUMN sale_start_date timestamp with time zone,
-ADD COLUMN sale_end_date timestamp with time zone;
